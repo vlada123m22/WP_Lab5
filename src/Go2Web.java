caeb12b6d169ab91e9c1ffdb2002cb0d1d223236
Vladislava Musin <135638760+vlada123m22@users.noreply.github.com>
2025-05-13 09:46:00 +0300

    Readme 2 (#8)
    * added -u tag
    
    * Implemented redirect
    
    * implemented cache mechanism
    
    * changed .gitignore
    
    * removed cache from .gitignore
    
    * removed cache from git
    
    * I guess I resolved the conflict
    
    * added TODO steps

--- src/Go2Web.java
+++ src/Go2Web.java
@@ -3,6 +3,8 @@ import java.net.*;
 import java.util.*;
 import java.util.regex.*;
 
+//TODO sockets, redirect, print when serving from cache
+//httpbin.org/redirect/{number} - for redirect
 public class
 Go2Web {
     private static final String CACHE_FILE = "cache.txt";
@@ -93,7 +95,8 @@ Go2Web {
 
     private static void searchWeb(String query) {
         if (isCached(query)) {
-            System.out.println("🔄 Serving search results from cache:");
+            System.out.println("Serving search results from cache:");
+
             displayResults(Arrays.asList(cache.get(query).split(";")));
             return;
         }
